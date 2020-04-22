d37f919f82d36f4256673cfb7ad3334a4476ab93
Mohamed Yassine <yassineaboub@live.fr>
2020-04-22 21:26:57 +0000

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,18 +1,26 @@
-from flask import Flask,render_template
+#!/usr/bin/python3
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+"""script that starts a Flask web application"""
+
 
 @app.route('/')
 def hello_HBNB():
     return 'Hello HBNB!'
+"""print Hello HBNB"""
+
 
 @app.route('/hbnb')
 def dsp_HBNB():
     return 'HBNB'
+"""print HBNB"""
+
 
 @app.route('/c/<text>')
 def c_HBNB(text):
     return ('C {}'.format(text.replace('_', ' ')))
+"""print  “C ” followed by value"""
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
