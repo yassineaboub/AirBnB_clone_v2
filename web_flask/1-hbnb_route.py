7f18b0918aa727398952dd00f961e3ebc1ec74dd
Mohamed Yassine <yassineaboub@live.fr>
2020-04-22 21:16:54 +0000

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+"""script that starts a Flask web application"""
+
 
 @app.route('/')
 def hello_HBNB():
     return 'Hello HBNB!'
+"""print HBNB"""
+
 
 @app.route('/hbnb')
 def dsp_HBNB():
@@ -12,4 +17,3 @@ def dsp_HBNB():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
